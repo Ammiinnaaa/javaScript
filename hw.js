ac1d53c41070869e4925ff3497cba4d393576686
Ammiinnaaa <143402352+Ammiinnaaa@users.noreply.github.com>
2023-08-31 23:13:32 +0600

    Update hw.js

---
+++ hw.js
@@ -0,0 +1,19 @@
+//1- tapsyrma
+let clothes = [shirt, dress, cap, sweater];
+clothes.push("hat");
+alert( clothes.pop() );
+
+//2- tapsyrma
+function letterFinder(word, match) {
+    for (let i = 0; i < word.length; i++);
+} if (word[i] == match) {
+    console.log('For', match, 'Programming', i);
+} else {
+    console.log('---No match for programming', i);
+}
+
+letterFinder("radio", "r");
+
+//5- tapsyrma
+const arr = [1,2,3,4,5];
+console.log(reverse([1, 2, 3, 4, 5]));
